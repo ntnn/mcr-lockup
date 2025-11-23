5a835903ff1701223ec760d242d49b0a8221c8c1
Nelo-T. Wallus <red.brush9525@fastmail.com>
2025-11-23 22:28:09 +0100

    Initial commit
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>

---
+++ main.go
@@ -0,0 +1,90 @@
+package mcrlockup
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/tools/clientcmd"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cluster"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	mctrl "sigs.k8s.io/multicluster-runtime"
+	mcbuilder "sigs.k8s.io/multicluster-runtime/pkg/builder"
+	mcreconcile "sigs.k8s.io/multicluster-runtime/pkg/reconcile"
+	"sigs.k8s.io/multicluster-runtime/providers/single"
+)
+
+func Run(ctx context.Context, kubeconfigs []string) error {
+	ctrl.SetLogger(zap.New(zap.UseDevMode(true)))
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	if len(kubeconfigs) == 0 {
+		return fmt.Errorf("at least one kubeconfig file must be specified")
+	}
+
+	kubeconfigBytes, err := os.ReadFile(kubeconfigs[0])
+	if err != nil {
+		return err
+	}
+
+	restConfig, err := clientcmd.RESTConfigFromKubeConfig(kubeconfigBytes)
+	if err != nil {
+		return err
+	}
+
+	cl, err := cluster.New(restConfig)
+	if err != nil {
+		return err
+	}
+
+	provider := single.New("cl", cl)
+
+	mcmanager, err := mctrl.NewManager(restConfig, provider, mctrl.Options{})
+	if err != nil {
+		return err
+	}
+
+	if err := mcmanager.GetLocalManager().Add(cl); err != nil {
+		return err
+	}
+
+	recv := make(chan struct{})
+
+	if err := mcbuilder.ControllerManagedBy(mcmanager).
+		For(&corev1.Namespace{}).
+		Complete(mcreconcile.Func(func(ctx context.Context, req mctrl.Request) (mctrl.Result, error) {
+			ctrl.Log.Info("reconciling namespace", "namespace", req.Namespace, "name", req.Name, "cluster", req.ClusterName)
+			recv <- struct{}{}
+			return mctrl.Result{}, nil
+		})); err != nil {
+		return err
+	}
+
+	go func() {
+		if err := mcmanager.Start(ctx); err != nil {
+			ctrl.Log.Error(err, "multicluster manager exited with error")
+		}
+	}()
+
+	timeoutCtx, timeoutCancel := context.WithTimeout(ctx, time.Second*10)
+	defer timeoutCancel()
+
+	select {
+	case <-ctx.Done():
+		ctrl.Log.Info("context cancelled before first reconcile event")
+		return nil
+	case <-recv:
+		cancel()
+		ctrl.Log.Info("received first reconcile event, shutting down")
+	case <-timeoutCtx.Done():
+		cancel()
+		ctrl.Log.Info("timeout waiting for first reconcile event, shutting down")
+	}
+
+	return nil
+}
